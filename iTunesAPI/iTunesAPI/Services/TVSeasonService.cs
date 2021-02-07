3a651e33530321665324830b71ed483d821bffde
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-02-07 17:46:20 +0200

    Released TVSeasonService

---
+++ iTunesAPI/iTunesAPI/Services/TVSeasonService.cs
@@ -0,0 +1,34 @@
+﻿using iTunesAPI.Models;
+using iTunesAPI.Services.Interfaces;
+using iTunesAPI.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace iTunesAPI.Services
+{
+    public class TVSeasonService : HttpRequester, ITVSeasonService
+    {
+        public async Task<TVSeasonsResult> GetTVSeasonById(long seasonId)
+        {
+            var nvc = HttpUrlBuilder.ParseQueryString(string.Empty);
+            nvc.Add("id", seasonId.ToString());
+            var result = await MakeRequestAsync<TVSeasonsResult>(nvc.ToString());
+            return result;
+        }
+
+        public async Task<TVSeasonsResult> GetTVSeasonsForShow(string showName, int resultLimit = 10, string countryCode = "us")
+        {
+            var nvc = HttpUrlBuilder.ParseQueryString(string.Empty);
+            nvc.Add("term", showName);
+            nvc.Add("media", "tvShow");
+            nvc.Add("entity", "tvSeason");
+            nvc.Add("attribute", "showTerm");
+            nvc.Add("limit", resultLimit.ToString());
+            nvc.Add("country", countryCode);
+            var result = await MakeRequestAsync<TVSeasonsResult>(nvc.ToString());
+            return result;
+        }
+    }
+}
