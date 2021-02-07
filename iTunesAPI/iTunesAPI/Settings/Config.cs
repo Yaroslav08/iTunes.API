3324a23c4a8a3ee908a861637c77e367a27c78c3
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-02-07 16:29:21 +0200

    Added config class

---
+++ iTunesAPI/iTunesAPI/Settings/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace iTunesAPI.Settings
+{
+    public struct Config
+    {
+        public static string BaseSearchUrl = "https://itunes.apple.com/search?";
+
+
+        public static string BaseLookupUrl = "https://itunes.apple.com/lookup?";
+    }
+}
