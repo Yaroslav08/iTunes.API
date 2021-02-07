91f8f8d298fb903fc067b2b2f56afa3549053599
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-02-07 17:40:25 +0200

    Released SongService

---
+++ iTunesAPI/iTunesAPI/Services/SongService.cs
@@ -0,0 +1,30 @@
+﻿using iTunesAPI.Models;
+using iTunesAPI.Services.Interfaces;
+using iTunesAPI.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTunesAPI.Services
+{
+    public class SongService : HttpRequester, ISongService
+    {
+        public async Task<SongsResult> GetSongsAsync(string song, int resultLimit = 100, string attribute = null, string countryCode = "us")
+        {
+            var nvc = HttpUrlBuilder.ParseQueryString(string.Empty);
+            nvc.Add("term", song);
+            nvc.Add("media", "music");
+            nvc.Add("entity", "musicTrack");
+            if (attribute != null)
+            {
+                nvc.Add("attribute", attribute);
+            }
+            nvc.Add("limit", resultLimit.ToString());
+            nvc.Add("country", countryCode);
+            var result = await MakeRequestAsync<SongsResult>(nvc.ToString());
+            return result;
+        }
+    }
+}
