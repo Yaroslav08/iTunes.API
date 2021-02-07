ca9f25dbe88686fdcb6af9e8c3f268709af5edd6
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-02-07 16:37:32 +0200

    Added AlbumResult model

---
+++ iTunesAPI/iTunesAPI/Models/AlbumResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+namespace iTunesAPI.Models
+{
+    public class AlbumResult
+    {
+        [JsonPropertyName("resultCount")]
+        public int Count { get; set; }
+
+        [JsonPropertyName("results")]
+        public List<Album> Albums { get; set; }
+    }
+}
