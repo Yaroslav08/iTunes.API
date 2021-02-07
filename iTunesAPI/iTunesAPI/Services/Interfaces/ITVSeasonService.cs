d43a81b3a1169e796dd6993309ae1d8f02bfb7bd
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-02-07 17:06:01 +0200

    Added methods to ITVSeasonService

--- iTunesAPI/iTunesAPI/Services/Interfaces/ITVSeasonService.cs
+++ iTunesAPI/iTunesAPI/Services/Interfaces/ITVSeasonService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using iTunesAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,7 @@ namespace iTunesAPI.Services.Interfaces
 {
     public interface ITVSeasonService
     {
-
+        Task<TVSeasonsResult> GetTVSeasonsForShow(string showName, int resultLimit = 10, string countryCode = "us");
+        Task<TVSeasonsResult> GetTVSeasonById(long seasonId);
     }
 }
