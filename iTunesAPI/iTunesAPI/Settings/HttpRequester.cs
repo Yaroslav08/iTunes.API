19bd320511b78fd2c4649e4cbc26a06d407f5e68
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-02-07 17:22:59 +0200

    Added HttpBuilder and Request class

---
+++ iTunesAPI/iTunesAPI/Settings/HttpRequester.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+namespace iTunesAPI.Settings
+{
+    public class HttpRequester
+    {
+        protected async Task<T> MakeRequstAsync<T>(string url)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(Config.BaseSearchUrl);
+            var result = await client.GetStringAsync(url);
+            var obj = JsonSerializer.Deserialize<T>(result);
+            return obj;
+        }
+    }
+}
