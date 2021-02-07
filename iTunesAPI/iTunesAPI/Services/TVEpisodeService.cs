b809a6362a57afebbda7c54ccb15d18d1b2af388
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-02-07 17:42:59 +0200

    Released TVEpisodeService

---
+++ iTunesAPI/iTunesAPI/Services/TVEpisodeService.cs
@@ -0,0 +1,26 @@
+﻿using iTunesAPI.Models;
+using iTunesAPI.Services.Interfaces;
+using iTunesAPI.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace iTunesAPI.Services
+{
+    public class TVEpisodeService : HttpRequester, ITVEpisodeService
+    {
+        public async Task<TVEpisodesResult> GetTVEpisodesForShow(string showName, int resultLimit = 100, string countryCode = "us")
+        {
+            var nvc = HttpUrlBuilder.ParseQueryString(string.Empty);
+            nvc.Add("term", showName);
+            nvc.Add("media", "tvShow");
+            nvc.Add("entity", "tvEpisode");
+            nvc.Add("attribute", "showTerm");
+            nvc.Add("limit", resultLimit.ToString());
+            nvc.Add("country", countryCode);
+            var result = await MakeRequestAsync<TVEpisodesResult>(nvc.ToString());
+            return result;
+        }
+    }
+}
