21348af6f6436762d206af6878338fecb0d638ed
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-02-07 17:38:11 +0200

    Released PodcastService

---
+++ iTunesAPI/iTunesAPI/Services/PodcastService.cs
@@ -0,0 +1,33 @@
+﻿using iTunesAPI.Models;
+using iTunesAPI.Services.Interfaces;
+using iTunesAPI.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace iTunesAPI.Services
+{
+    public class PodcastService : HttpRequester, IPodcastService
+    {
+        public async Task<PodcastsResult> GetPodcastById(long podcastId)
+        {
+            var nvc = HttpUrlBuilder.ParseQueryString(string.Empty);
+            nvc.Add("id", podcastId.ToString());
+            var result = await MakeRequestAsync<PodcastsResult>(nvc.ToString());
+            return result;
+        }
+
+        public async Task<PodcastsResult> GetPodcasts(string podcast, int resultLimit = 100, string countryCode = "us")
+        {
+            var nvc = HttpUrlBuilder.ParseQueryString(string.Empty);
+            nvc.Add("term", podcast);
+            nvc.Add("media", "podcast");
+            nvc.Add("attribute", "titleTerm");
+            nvc.Add("limit", resultLimit.ToString());
+            nvc.Add("country", countryCode);
+            var result = await MakeRequestAsync<PodcastsResult>(nvc.ToString());
+            return result;
+        }
+    }
+}
