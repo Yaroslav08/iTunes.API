7b5d209b539f4d90b74f70e80440b826556295c5
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-02-07 17:11:38 +0200

    Init basic interfaces

--- iTunesAPI/iTunesAPI/iTunesClient.cs
+++ iTunesAPI/iTunesAPI/iTunesClient.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using iTunesAPI.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 namespace iTunesAPI
@@ -8,5 +10,30 @@ namespace iTunesAPI
     public class iTunesClient
     {
 
+        private HttpClient client;
+
+        private IAlbumService album;
+        private IArtistService artist;
+        private IPodcastService podcast;
+        private ISongService song;
+        private ITVEpisodeService tvEposide;
+        private ITVSeasonService tvSeason;
+
+        public iTunesClient(IAlbumService album, IArtistService artist, IPodcastService podcast, ISongService song, ITVEpisodeService tvEposide, ITVSeasonService tvSeason)
+        {
+            this.album = album;
+            this.artist = artist;
+            this.podcast = podcast;
+            this.song = song;
+            this.tvEposide = tvEposide;
+            this.tvSeason = tvSeason;
+        }
+
+        public IAlbumService Album => album;
+        public IArtistService Artist => artist;
+        public IPodcastService Podcast => podcast;
+        public ISongService Song => song;
+        public ITVEpisodeService TvEposide => tvEposide;
+        public ITVSeasonService TvSeason => tvSeason;
     }
 }
