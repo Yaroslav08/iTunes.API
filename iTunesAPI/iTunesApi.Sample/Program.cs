f458df1751e93c71b9cd5aaf2f35d722f26608d9
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-02-07 19:11:22 +0200

    Create a loader music preview

--- iTunesAPI/iTunesApi.Sample/Program.cs
+++ iTunesAPI/iTunesApi.Sample/Program.cs
@@ -1,5 +1,8 @@
 ﻿using iTunesAPI;
 using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace iTunesApi.Sample
 {
@@ -8,9 +11,35 @@ namespace iTunesApi.Sample
         static async System.Threading.Tasks.Task Main(string[] args)
         {
             iTunesClient client = new iTunesClient();
-            var res = await client.Song.GetSongsAsync("Roses");
+            var res = await client.Song.GetSongsAsync("Roses", 200);
+            foreach (var item in res.Songs)
+            {
+                RunPreviewMusic(item.PreviewUrl);
+            }
 
+            //var song = res.Songs.FirstOrDefault(d => d.ArtistName.Contains("SAINt"));
+            //RunPreviewMusic(song.PreviewUrl);
+        }
 
+        static void RunPreviewMusic(string url)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                url = url.Replace("&", "^&");
+                Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Process.Start("xdg-open", url);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                Process.Start("open", url);
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
         }
     }
 }
